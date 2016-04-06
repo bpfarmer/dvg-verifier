ccbbff2ece47ae332f8641791a7e91c6a8ede4a3
Brendan Farmer <bpfarmer@gmail.com>
2016-04-05 22:59:16 -0700

    replacing sha256 lib and tested hash as working

--- extension/popup.js
+++ extension/popup.js
@@ -3,25 +3,17 @@
 
 // Make XHR to receive binary data
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE) {
-        // Hash binary data
-        var data = new Uint8Array(xhr.response || xhr.mozResponseArrayBuffer);
-        console.log(data);
-        //PDFJS.getDocument(data).then(function(pdf) {
-        //  console.log(pdf);
-        //});
-        //console.log(pdf)
-        var shaObj = new jsSHA("SHA-256", "BYTES");
-        shaObj.update(this.response);
-        var hash = shaObj.getHash("HEX");
-        console.log(hash);
-    }
+xhr.onload = function(e) {
+  var data = new Uint8Array(xhr.response);
+  console.log(data);
+  var hash = sha256(data);
+  console.log(hash);
 }
+
 console.log($("embed").attr("src"));
-xhr.responseType = 'arraybuffer';
 xhr.open('GET', $("embed").attr("src"), true);
-xhr.send(null);
+xhr.responseType = 'arraybuffer';
+xhr.send();
 
 // Use PDF.js or PDFium to parse metadata
 
