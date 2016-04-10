95c657a4c100e3eee664db5e8d3e60a7785b3419
Brendan Farmer <bpfarmer@gmail.com>
2016-04-09 17:10:50 -0700

    changing request path structure

--- transparency.go
+++ transparency.go
@@ -8,11 +8,10 @@ import (
 )
 
 func verifyReq(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Request: %s!", r.URL.Path[2:])
+	fmt.Fprintf(w, "Request: %s!", r.URL.Path[1:])
 }
 
 func main() {
-
-	http.HandleFunc("/verify", verifyReq)
+	http.HandleFunc("/verify/", verifyReq)
 	http.ListenAndServe(":4000", nil)
 }
